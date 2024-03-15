20953ab1cbb8095db367e4933872cf63d7c5be01
Ankit <ankitwarade023@gmail.com>
2024-03-15 18:06:13 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import {ApiResponse} from "./middleware/ApiResponse.js";
+import {ApiResponse} from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler(async (req, res) => {
     // create a model to take user data. add validation
@@ -12,22 +12,25 @@ const registerUser = asyncHandler(async (req, res) => {
     // save to database and return the success message.
 
     const { fullName, email, username, password } = req.body
-    console.log(`email: ${email}`)
 
     if ([fullName, email, username, password].some((field) => field?.trim() === "")) {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ email }, { username }]
     })
 
-    if (existerUser){
+    if (existedUser){
         throw new ApiError(409, "User with email or username already exits")
     }
     //console log to check what is comming here
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file required")
